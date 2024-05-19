5caddfd2ff52bb6376d7724c1e77ce4505c3dc0d
Deepak Rizal <deepak2000rizal@gmail.com>
2024-05-19 14:18:45 +0530

    made changes due to some error

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -5,7 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { postMessage } from "../services/chatService";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://universal-chatroom-app-backend.onrender.com");
 
 const MessageInput = () => {
   const [message, setMessage] = useState("");
